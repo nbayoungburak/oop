5a02fbe94145f988d80d879c7ec0713ceceb4ee3
nbayoungburak <burakcan.odms@gmail.com>
2022-03-08 14:33:36 +0300

    oop

---
+++ lab1/src/Professors.java
@@ -0,0 +1,14 @@
+public class Professors {
+
+String prof_name;
+
+Professors(String name){
+    prof_name=name;
+}
+void teaches(){
+
+    System.out.println("prof "+ prof_name);
+}
+
+
+}
---
+++ lab1/src/student.java
@@ -0,0 +1,21 @@
+public class student {
+
+    String std_name;
+
+    student(String name){
+        std_name=name;
+    }
+    void learns(){
+
+        System.out.println("student"+ std_name);
+    }
+
+
+
+
+
+
+
+}
+
+
